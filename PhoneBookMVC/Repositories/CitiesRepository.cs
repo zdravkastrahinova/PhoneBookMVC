8c038e603787cac45f75598823248ab331220467
Zdravka Strahinova <Zdravka Strahinova>
2016-04-15 14:46:56 +0300

    Add repositories for countries and cities

---
+++ PhoneBookMVC/Repositories/CitiesRepository.cs
@@ -0,0 +1,14 @@
+﻿using PhoneBookMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBookMVC.Repositories
+{
+    public class CitiesRepository : BaseRepository<City>
+    {
+        public CitiesRepository() : base() { }
+        public CitiesRepository(UnitOfWork unitOfWork) : base(unitOfWork) { }
+    }
+}
---
+++ PhoneBookMVC/Repositories/CountriesRepository.cs
@@ -0,0 +1,14 @@
+﻿using PhoneBookMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBookMVC.Repositories
+{
+    public class CountriesRepository : BaseRepository<Country>
+    {
+        public CountriesRepository() : base() { }
+        public CountriesRepository(UnitOfWork unitOfWork) : base(unitOfWork) { }
+    }
+}
